dbe6782040ca4ec14796ad8104f3858e9b2659cb
Olexandr Ivaschenko <sasaivasenko9@gmail.com>
2025-05-14 17:12:15 +0300

    create Post, User, Comment components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import './App.scss';
 
-// import postsFromServer from './api/posts.json';
-// import commentsFromServer from './api/comments.json';
-// import usersFromServer from './api/users.json';
+import postsFromServer from './api/posts.json';
+import commentsFromServer from './api/comments.json';
+import usersFromServer from './api/users.json';
+import { PostList } from './components/PostList/PostList';
+
+function getUserById(userId) {
+  return usersFromServer.find(user => user.id === userId) || null;
+}
+
+export const posts = postsFromServer.map(post => ({
+  ...post,
+  user: getUserById(post.userId),
+  comments: commentsFromServer.filter(comment => comment.postId === post.id),
+}));
 
 export const App = () => (
   <section className="App">
     <h1 className="App__title">Static list of posts</h1>
+    <PostList posts={posts} />
 
     <div className="PostList">
       <div className="PostInfo">
